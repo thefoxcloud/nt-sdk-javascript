32d78679a4d006bf299595f4bb13ec0ab053d17e
KayoChan <kayo@foxysun.com>
2021-05-06 21:22:06 +0700

    Updated type

--- src/Model/IBoatRamp.ts
+++ src/Model/IBoatRamp.ts
@@ -13,3 +13,16 @@ export default interface IBoatRamp {
   parking: string;
   accessible: string;
 };
+
+export interface IBoatRampOptional {
+  id?: string;
+  title?: string;
+  location?: ICoordinate;
+  streetAddress?: string;
+  region?: string;
+  description?: string;
+  alert?: IAlertSystem;
+  signOnly?: boolean;
+  parking?: string;
+  accessible?: string;
+}
