d0b9a4d32cd31379fff517a7a37ad55135134460
KayoTran <kayo@foxysun.com>
2021-12-13 13:45:17 +0700

    Added notification type

---
+++ src/Enum/NotificationType.ts
@@ -0,0 +1,10 @@
+enum NotificationType {
+  Message = 'message',
+  Weather = 'weather',
+  Post = 'post',
+  Fish = 'fish',
+  Zone = 'zone',
+  Point = 'point'
+};
+
+export default NotificationType;
